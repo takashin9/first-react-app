5399cb4a9f31f38399eb88353c241d18edf7007c
takashin9 <takano.s.293@ms.saitama-u.ac.jp>
2023-06-19 19:15:40 +0900

    フォームの値を管理するための状態を作成

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-import Counter from "./components/Counter";
+import { useState } from "react";
 
 function App() {
+  const [todoInput, setTodoInput] = useState("");
+
+  function handleChange(event) {
+    setTodoInput(event.target.value);
+  };
+
   return (
-    <Counter />
+    <div>
+      <input type="text" value={todoInput} onChange={handleChange} />
+      <p>{todoInput}</p>
+    </div>
   );
 };
 
-
-
-export default App
+export default App;
